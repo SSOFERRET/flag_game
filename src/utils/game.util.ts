9423ad9adcbaa7af2daef3a3d3a33196bb2d5582
SSOFERRET <owosso1212@gmail.com>
2024-11-06 15:42:43 +0900

    Feat: getSide, getCommand, getScriptString 함수를 getGameCommand 함수 안에 통합

--- src/utils/game.util.ts
+++ src/utils/game.util.ts
@@ -1,21 +1,41 @@
 import { downCommand, upCommand } from "../constants/commands.constant";
-import { ICommand, IFlagSelected, TSide } from "../models/game.model";
+import { flags } from "../constants/flags.constant";
+import { ICommand, ISelectedFlag, TSide } from "../models/game.model";
 import { getRandomNumber } from "./random.util";
 
-const getSide = (): TSide => {
-  const side: TSide[] = ["left", "right"];
-  return side[getRandomNumber(0, 1)];
+interface IGameCommand {
+  side: TSide;
+  command: ICommand;
+  script: string;
 }
 
-const getCommand = (): ICommand | null => {
-  const upOrDown: number = getRandomNumber(0, 1);
-  switch (upOrDown) {
-    case 0: return upCommand[getRandomNumber(0, upCommand.length - 1)];
-    case 1: return downCommand[getRandomNumber(0, downCommand.length - 1)];
-    default: return null;
+const getGameCommand = (): IGameCommand => {
+  const getSide = (): TSide => {
+    const side: TSide[] = ["left", "right"];
+    return side[getRandomNumber(0, 1)];
   }
-}
+  
+  const getCommand = (): ICommand | null => {
+    const upOrDown: number = getRandomNumber(0, 1);
+    switch (upOrDown) {
+      case 0: return upCommand[getRandomNumber(0, upCommand.length - 1)];
+      case 1: return downCommand[getRandomNumber(0, downCommand.length - 1)];
+      default: return null;
+    }
+  }
+  
+  const getScriptString = (side: TSide, command: ICommand, selectedFlag: ISelectedFlag): string => {
+    return `${selectedFlag[side].name} ${command.script}`
+  }
 
-const getScriptString = (side: TSide, command: ICommand, flags: IFlagSelected): string => {
-  return `${flags[side].name} ${command.script}`
-}
+  const side: TSide = getSide();
+  const command: ICommand | null = getCommand();
+  if (!command) throw new Error("Command Selection Error");
+  const selectedFlag: ISelectedFlag = {
+    left: flags[0],
+    right: flags[1],
+  }
+  const script: string = getScriptString(side, command, selectedFlag);
+
+  return {side, command, script};
+};
