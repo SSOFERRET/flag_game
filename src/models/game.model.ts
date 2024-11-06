330cdf6cce91164dea43c31a6242e3348ba9f844
SSOFERRET <owosso1212@gmail.com>
2024-11-06 14:57:27 +0900

    Feat: interface ICommand 및 type TState 정의 #4

--- src/models/game.model.ts
+++ src/models/game.model.ts
@@ -1,1 +1,10 @@
 export type TSide = "left" | "right";
+
+export type TState = "up" | "down";
+
+export interface ICommand {
+    id: number;
+    script: string;
+    prevState?: TState;
+    result: TState;
+}
