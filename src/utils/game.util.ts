200ce491dcb331baa56634549e9566dc77694998
SSOFERRET <owosso1212@gmail.com>
2024-11-06 14:58:53 +0900

    Feat: getCommand 함수 작성 #4

--- src/utils/game.util.ts
+++ src/utils/game.util.ts
@@ -1,7 +1,17 @@
+import { downCommand, upCommand } from "../constants/commands.constant";
 import { TSide } from "../models/game.model";
 import { getRandomNumber } from "./random.util";
 
 const getSide = (): TSide => {
   const side: TSide[] = ["left", "right"];
   return side[getRandomNumber(0, 1)];
+}
+
+const getCommand = (): ICommand | null => {
+  const upOrDown: number = getRandomNumber(0, 1);
+  switch (upOrDown) {
+    case 0: return upCommand[getRandomNumber(0, upCommand.length - 1)];
+    case 1: return downCommand[getRandomNumber(0, downCommand.length - 1)];
+    default: return null;
+  }
 }
