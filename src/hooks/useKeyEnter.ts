28e9f7db8914077db33d635f75a78a657c338d2c
ssoferret <owosso1212@gmail.com>
2024-11-09 10:45:00 +0900

    Feat: 게임 시작 페이지에서 `Enter` 키를 치면 게임 페이지로 Route 되는 훅 작성 #20

---
+++ src/hooks/useKeyEnter.ts
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const useKeyEnter = () => {
+  const nav = useNavigate();
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Enter")
+        nav('/game', { replace: true });
+    }
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    }
+  }, []);
+}
+
+export default useKeyEnter;
