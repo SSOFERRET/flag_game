5aed1c40b712c5781652a899d5e12f5f5bc2738f
ssoferret <owosso1212@gmail.com>
2024-11-09 10:42:04 +0900

    Rename: 스토어 인터페이스 명 변경 반영 ICustomSlice > ISlice #20

--- src/stores/customSlice.store.ts
+++ src/stores/customSlice.store.ts
@@ -1,13 +1,12 @@
 import { StateCreator } from "zustand";
+import { ISlice } from "./boundStore.store";
 
-export type ICustomSlice = INameCustomSlice
-
-interface INameCustomSlice {
+export interface INameCustomSlice {
     userName: string;
     setUserName: (userName: string) => void;
 }
 
-export const userNameSlice: StateCreator<ICustomSlice, [], [], INameCustomSlice> = (set) => ({
+export const userNameSlice: StateCreator<ISlice, [], [], INameCustomSlice> = (set) => ({
     userName: "오똑씨",
-    setUserName: (userName: string) => set(() => ({userName})),
+    setUserName: (userName: string) => set(() => ({ userName })),
 })
