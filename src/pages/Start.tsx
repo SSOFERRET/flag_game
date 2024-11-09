3efe8ba5a1752ca60effbf5f2535e120d74a406c
ssoferret <owosso1212@gmail.com>
2024-11-09 10:43:42 +0900

    Feat: 게임 시작 페이지에 토글 버튼 배치 #20

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -2,19 +2,27 @@ import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import boundStore from "../stores/boundStore.store";
 import "./Start.css"
+import useToggleLeftRight from "../hooks/useToggleLeftRight";
+import useKeyEnter from "../hooks/useKeyEnter";
 
 function Start() {
   const userName = boundStore.use.userName();
   const nav = useNavigate();
+  const {onClickLeft, onClickRight} = useToggleLeftRight();
+  useKeyEnter();
 
   return (
     <div className="Start">
-        <section className="userName">
-            {userName}
-        </section>
-        <section className="startButton">
+      <section className="toggleButton">
+        <button className="leftButton" onClick={onClickLeft}></button>
+        <button className="rightButton" onClick={onClickRight}></button>
+      </section> 
+      <section className="userName">
+        {userName}
+      </section>
+      <section className="startButton">
             <Button text="게임 시작!" onClick={() => {nav('/game')}}/>
-        </section>
+      </section>
     </div>
   )
 }
