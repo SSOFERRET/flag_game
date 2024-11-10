bbf91662815bbd887c7335f35395311c9085e488
ssoferret <owosso1212@gmail.com>
2024-11-10 20:01:38 +0900

    Feat: 캐릭터 컴포넌트 생성 및  css

---
+++ src/components/Character.tsx
@@ -0,0 +1,26 @@
+import "./Character.css"
+import headImg from "./../assets/head.png";
+import leftDownImg from "./../assets/leftDown.png";
+import leftUpImg from "./../assets/leftUp.png";
+import rightDownImg from "./../assets/rightDown.png";
+import rightUpImg from "./../assets/rightUp.png";
+import legImg from "./../assets/legs.png";
+import boundStore from "../stores/boundStore.store";
+
+const Character = () => {
+  const left = boundStore.use.left();
+  const right = boundStore.use.right();
+
+  return (
+    <div className="Character">
+      <section className="body">
+        <img className="head" src={headImg} />
+        <img className={`left left_${left}`} src={left==="down" ? leftDownImg : leftUpImg} />
+        <img className={`right right_${right}`} src={right==="down" ? rightDownImg : rightUpImg} />
+      </section>
+      <img className="legs" src={legImg} />
+    </div>
+  )
+}
+
+export default Character;
