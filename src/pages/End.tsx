78d6e9ea83e5fde200e00be3494386dbe5fdc5f1
SSOFERRET <owosso1212@gmail.com>
2024-11-11 18:12:27 +0900

    Feat: 게임 종료 페이지에 공유 이미지 컴포넌트 및 버튼 컴포넌트 배치

--- src/pages/End.tsx
+++ src/pages/End.tsx
@@ -1,9 +1,14 @@
+import Button from "../components/Button";
+import ShareImage from "../components/ShareImage";
 import "./End.css";
 
 const End = () => {
     return (
         <div className="End">
-            End
+            <ShareImage />
+            <section className="shareButtons">
+                <Button text="X에 공유하기" onClick={() => {}}/>
+            </section>
         </div>
     )
 };
