aa9eda7624d4e5e4076d071b617f3e2cb1c1b076
ssoferret <owosso1212@gmail.com>
2024-11-14 20:21:07 +0900

    Feat: 게임 종료 페이지 작성

--- src/pages/End.tsx
+++ src/pages/End.tsx
@@ -1,13 +1,29 @@
+import { useRef } from "react";
 import Button from "../components/Button";
-import ShareImage from "../components/ShareImage";
+import Card from "../components/Card";
 import "./End.css";
+import html2canvas from "html2canvas";
 
 const End = () => {
+    const cardRef = useRef<HTMLDivElement>(null);
+
+    const onClickSaveImage = () => {
+        if (cardRef.current) {
+            html2canvas(cardRef.current).then((canvas) => {
+                const imgData = canvas.toDataURL("image/png");
+                const link = document.createElement("a");
+                link.href = imgData;
+                link.download = "card.png";
+                link.click();
+              });
+            }
+          };
+
     return (
         <div className="End">
-            <ShareImage />
+            <Card ref={cardRef} />
             <section className="shareButtons">
-                <Button text="X에 공유하기" onClick={() => {}}/>
+                <Button text="이미지로 저장" onClick={onClickSaveImage}/>
             </section>
         </div>
     )
