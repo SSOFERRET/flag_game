00c15672c80fae435c15d2d0fad9b2db9868b54b
SSOFERRET <owosso1212@gmail.com>
2024-11-15 18:08:36 +0900

    Feat: 시작 페이지에 레이아웃 컴포넌트 적용

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,31 +1,21 @@
-import "./Start.css"
 import { useNavigate } from "react-router-dom";
-import useToggleLeftRight from "../hooks/useToggleLeftRight";
-import useKeyEnter from "../hooks/useKeyEnter";
-import Character from "../components/Character";
 import title from "../assets/title2.png";
 import button2 from "../assets/button2.png";
+import Layout from "../components/Layout";
 
 function Start() {
   const nav = useNavigate();
-  const {onClickLeft, onClickRight} = useToggleLeftRight();
-  useKeyEnter();
 
   return (
-    <div className="Start">
-      <section className="toggleButton">
-        <button className="leftButton" onClick={onClickLeft}></button>
-        <button className="rightButton" onClick={onClickRight}></button>
-      </section> 
-      <section className="title">
-        <img src={title} />
-      </section>
-      <Character />
-      <section className="startButton" onClick={() => nav("/game")}>
-        <div className="buttonText">게임 시작!</div>
-        <img src={button2}/>
-      </section>
-    </div>
+    <Layout 
+      headChild={<img src={title} />}
+      footChild={(
+        <section className="startButton" onClick={() => nav("/game")}>
+          <div className="buttonText">게임 시작!</div>
+          <img src={button2}/>
+        </section>
+      )}
+    />
   )
 }
 
