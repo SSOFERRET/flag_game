7212515274546bc7233d3f03caccebad7c439a0b
ssoferret <owosso1212@gmail.com>
2024-11-17 10:59:54 +0900

    Feat: 새 슬라이스 스토어에 반영

--- src/stores/boundStore.store.ts
+++ src/stores/boundStore.store.ts
@@ -2,12 +2,14 @@ import { create } from "zustand";
 import createSelectors from ".";
 import { INameCustomSlice, userNameSlice } from "./customSlice.store";
 import { IMotionSlice, motionSlice } from "./motionSlice.store";
+import { gameSlice, IGameSlice } from "./gameStore.store";
 
-export type ISlice = INameCustomSlice & IMotionSlice
+export type ISlice = INameCustomSlice & IMotionSlice & IGameSlice
 
 const boundStoreBase = create<ISlice>()((...a) => ({
     ...userNameSlice(...a),
     ...motionSlice(...a),
+    ...gameSlice(...a),
 }));
 
 export default createSelectors(boundStoreBase);
