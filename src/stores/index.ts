9780a53bceef50716af6fe93ba7a421beb8ed737
ssoferret <owosso1212@gmail.com>
2024-11-17 13:28:09 +0900

    Fix: createSelectors 타입 오류 수정

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,19 +1,22 @@
-import { StoreApi, UseBoundStore } from 'zustand'
+import { StoreApi, UseBoundStore } from 'zustand';
 
 type WithSelectors<S> = S extends { getState: () => infer T }
   ? S & { use: { [K in keyof T]: () => T[K] } }
-  : never
+  : never;
 
-const createSelectors = <S extends UseBoundStore<StoreApi<object>>>(
+const createSelectors = <T extends object, S extends UseBoundStore<StoreApi<T>>>(
   _store: S,
 ) => {
-  const store = _store as WithSelectors<typeof _store>
-  store.use = {}
-  for (const k of Object.keys(store.getState())) {
-    ;(store.use as T)[k] = () => store((s) => s[k as keyof typeof s])
+  type StoreState = T;
+  const store = _store as WithSelectors<S>;
+
+  store.use = {} as { [K in keyof StoreState]: () => StoreState[K] };
+
+  for (const k of Object.keys(store.getState()) as (keyof StoreState)[]) {
+    store.use[k] = () => store((s) => s[k]);
   }
 
-  return store
-}
+  return store;
+};
 
 export default createSelectors;
