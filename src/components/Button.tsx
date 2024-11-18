3d31bd2c0d29e7f44026112ce6a174a3a3b12fb2
SSOFERRET <owosso1212@gmail.com>
2024-11-18 17:27:56 +0900

    Refact: 버튼 컴포넌트 글자 크기 프로퍼티 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,19 @@ import style from "./Button.module.css";
 interface IProps {
     text: string;
     onClick: () => void;
+    textSize: "small" | "middle" | "large";
 }
 
-const Button = ({text, onClick}: IProps) => {
+const Button = ({text, onClick, textSize="large"}: IProps) => {
     return (
-        <button className={style.button} onClick={onClick}>{text}</button>
+        <button
+          className={textSize === "large" ? style.buttonLarge
+            : textSize === "middle" ? style.buttonMiddle
+            : style.buttonSmall}
+          onClick={onClick}
+        >
+            {text}
+        </button>
     )
 }
 
