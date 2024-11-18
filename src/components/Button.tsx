3a9e837cea2b55e088f736cda6ed8bb7e753c94f
ssoferret <owosso1212@gmail.com>
2024-11-18 21:23:27 +0900

    Fix: 버튼 컴포넌트 프로퍼티 에러 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import style from "./Button.module.css";
 interface IProps {
     text: string;
     onClick: () => void;
-    textSize: "small" | "middle" | "large";
+    textSize?: "small" | "middle" | "large";
 }
 
 const Button = ({text, onClick, textSize="large"}: IProps) => {
