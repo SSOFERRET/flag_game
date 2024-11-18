1ca0a32cf96f4e24381da86e7ffb8690f11d3dbe
ssoferret <owosso1212@gmail.com>
2024-11-18 09:19:19 +0900

    Feat: 스크립트 배열 변경사항 반영하여 게임 기능 수정

--- src/utils/game.util.ts
+++ src/utils/game.util.ts
@@ -1,51 +1,78 @@
-import { downCommand, upCommand } from "../constants/commands.constant";
+import { commands } from "../constants/commands.constant";
 import { flags } from "../constants/flags.constant";
-import { ICommand, ISelectedFlag, IState, TSide } from "../models/game.model";
+import { ICommand, ISelectedFlag, IState, TSide, TState } from "../models/game.model";
 import { getRandomNumber } from "./random.util";
 
 interface IGameCommand {
   side: TSide;
   command: ICommand;
   script: string;
+  sounds: string[];
 }
 
 export const getGameCommand = (): IGameCommand => {
   const getSide = (): TSide => {
-    const side: TSide[] = ["left", "right"];
-    return side[getRandomNumber(0, 1)];
-  }
-  
-  const getCommand = (): ICommand | null => {
-    const upOrDown: number = getRandomNumber(0, 1);
-    switch (upOrDown) {
-      case 0: return upCommand[getRandomNumber(0, upCommand.length - 1)];
-      case 1: return downCommand[getRandomNumber(0, downCommand.length - 1)];
-      default: return null;
+    const randomNumber = getRandomNumber(0, 4);
+
+    switch (randomNumber) {
+      case 0: return "left";
+      case 1: return "right";
+      case 2: return "left";
+      case 3: return "right";
+      case 4: return "both";
+      default: return "both";
     }
   }
-  
-  const getScriptString = (side: TSide, command: ICommand, selectedFlag: ISelectedFlag): string => {
-    return `${selectedFlag[side].name} ${command.script}`
+
+  const getCommand = (): ICommand => {
+    const randomNumber = getRandomNumber(0, commands.length - 1);
+
+    return commands[randomNumber];
   }
 
-  const side: TSide = getSide();
+  const getScriptString = (side: TSide, command: ICommand, selectedFlag: ISelectedFlag): string => {
+    return `${selectedFlag[side].name}, ${command.script}`
+  }
+
+  const getScriptSound = (side: TSide, command: ICommand, selectedFlag: ISelectedFlag): string[] => {
+    return [selectedFlag[side].sound, command.sound];
+  }
+
+  const side = getSide();
   const command: ICommand | null = getCommand();
   if (!command) throw new Error("Command Selection Error");
+
   const selectedFlag: ISelectedFlag = {
     left: flags[0],
     right: flags[1],
+    both: flags[2],
   }
-  const script: string = getScriptString(side, command, selectedFlag);
 
-  return {side, command, script};
+  const script = getScriptString(side, command, selectedFlag);
+  const sounds = getScriptSound(side, command, selectedFlag);
+
+  return { side, command, script, sounds };
 };
 
 export const getJudge = (prevState: IState, currentState: IState, side: TSide, command: ICommand) => {
-  const leftAnswer = side === "left" ? command.result : prevState.left;
-  const rightAnswer = side === "right" ? command.result : prevState.right;
+  let leftAnswer: TState = prevState.left;
+  let rightAnswer: TState = prevState.right;
+
+  if (side === "both" && command.result !== "stay") {
+    leftAnswer = command.result;
+    rightAnswer = command.result;
+  }
+
+  if (side === "left" && command.result !== "stay")
+    leftAnswer = command.result;
+
+  if (side === "right" && command.result !== "stay")
+    rightAnswer = command.result;
+
+
   console.log("답", {
-    left: leftAnswer,
-    right: rightAnswer
+    leftAnswer,
+    rightAnswer
   })
 
   if (currentState.left !== leftAnswer)
