25bdae171c8a8a6baad80ce4bb86e4a28f19b7fe
SSOFERRET <owosso1212@gmail.com>
2024-11-18 17:34:41 +0900

    Feat: 음성 재생 준비 안내창 추가

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -4,26 +4,37 @@ import button2 from "../assets/images/button2.webp";
 import Layout from "../components/Layout";
 import useKeyEnter from "../hooks/useKeyEnter";
 import useStartGame from "../hooks/useStartGame";
+import soundTest from "./../assets/scripts/01_blue.mp3";
+import Modal from "../components/Modal";
+import boundStore from "../stores/boundStore.store";
 
 function Start() {
   useKeyEnter();
   const startGame = useStartGame();
+  const setAudio = boundStore.use.setAudio();
 
   return (
-    <Layout 
-      headChild={
-        <section className={style.title}> 
-          <img src={title} />
-        </section>
-      }
-      footChild={(
-        <section className={style.startButton} onClick={startGame}>
-          <div className={style.buttonText}>게임 시작!</div>
-          <img src={button2}/>
-        </section>
-      )}
-      onClick={()=>{console.log("마이 보디!!!")}}
-    />
+    <div className={style.Start}>
+      <Modal 
+        text="음성이 재생됩니다!"
+        buttonText="준비됐어요!"
+        onClick={() => setAudio(soundTest)}
+       />
+      <Layout 
+        headChild={
+          <section className={style.title}> 
+            <img src={title} />
+          </section>
+        }
+        footChild={(
+          <section className={style.startButton} onClick={startGame}>
+            <div className={style.buttonText}>게임 시작!</div>
+            <img src={button2}/>
+          </section>
+        )}
+        onClick={()=>{console.log("마이 보디!!!")}}
+      />
+    </div>
   )
 }
 
