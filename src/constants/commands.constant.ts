02d732ba2b0beee7935075a11f62e05f13b53330
ssoferret <owosso1212@gmail.com>
2024-11-18 21:22:17 +0900

    Chore: 음원 파일 변경사항 반영

--- src/constants/commands.constant.ts
+++ src/constants/commands.constant.ts
@@ -1,25 +1,25 @@
 import { ICommand } from "../models/game.model";
-import up1 from "./../assets/scripts/04_up1.mp3";
-import down1 from "./../assets/scripts/08_down1.mp3";
-import stay1 from "./../assets/scripts/12_stay1.mp3";
+import up from "./../assets/scripts/04_up.mp3";
+import down from "./../assets/scripts/05_down.mp3";
+import stay from "./../assets/scripts/06_stay.mp3";
 
 export const commands: ICommand[] = [
   {
     id: 0,
     script: "올려",
     result: "up",
-    sound: up1,
+    sound: up,
   },
   {
     id: 1,
     script: "내려",
     result: "down",
-    sound: down1,
+    sound: down,
   },
   {
     id: 2,
     script: "그대로",
     result: "stay",
-    sound: stay1,
+    sound: stay,
   },
 ]
