4c72d19cb2dd95dd6b2bfb0ad53ad3e935a40d35
ssoferret <owosso1212@gmail.com>
2024-11-18 09:18:21 +0900

    Feat: 스크립트 constants 객체에 사운드 소스 프로퍼티 추가

--- src/models/game.model.ts
+++ src/models/game.model.ts
@@ -1,23 +1,28 @@
-export type TSide = "left" | "right";
+export type TSide = "left" | "right" | "both";
 
 export type TState = "up" | "down";
+
+export type TResult = "up" | "down" | "stay";
 
 export interface ICommand {
     id: number;
     script: string;
     prevState?: TState;
-    result: TState;
+    result: TResult;
+    sound: string;
 }
 
 export interface IFlag {
     id: number;
     color: string;
     name: string;
+    sound: string;
 }
 
 export interface ISelectedFlag {
     left: IFlag;
     right: IFlag;
+    both: IFlag;
 }
 
 export interface IState {
